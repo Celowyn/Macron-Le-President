72ea9bcd61df171e9a9280c8b28540cba83ff921
Koronaga <33987492+Koronaga@users.noreply.github.com>
2017-12-05 03:30:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,8 +19,8 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    // It will listen for messages that will start with `m!`
+    if (message.substring(0, 1) == 'm!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
 
